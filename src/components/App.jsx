c09a0500da12f01df3d8377a8f6a340ce8e7d9d3
Vladyslav Hurzhyi <soulline0@gmail.com>
2022-11-10 22:31:43 +0100

    profile without css

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,6 @@
+import { Profile } from './Profile/Profile';
+import user from './Profile/user.json';
+
 export const App = () => {
   return (
     <div
@@ -7,10 +10,16 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
-      React homework template
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
     </div>
   );
 };
